00653863250b824627d2e0c4d3cd35c002d31df6
mjrafi01 <61502480+mjrafi01@users.noreply.github.com>
2021-02-03 16:48:38 -0800

    Started

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodedcdfdfSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happen!it's gonna be fun</h2>
     </div>
   );
 }
